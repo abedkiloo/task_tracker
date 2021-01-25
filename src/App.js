d6bb618aa3471d3a1c51e849fb5439dae79c08c4
Abednego Wambua <abedxh@gmail.com>
2021-01-25 10:48:01 +0300

    adding prp types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <h2>Class Nmae</h2>
+      <Header title="Task Tracker"/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Header = ()=>{
+const Header = ({title})=>{
     return (
-        <div>
+        <header>
+            <h1 style={{
+                color:"#3e3ede",
             
-        </div>
+            }}>{title}</h1>
+        </header>
     )
 }
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+Header.propTypes= {
+    title: PropTypes.string.isRequired,
+}
+export default Header
